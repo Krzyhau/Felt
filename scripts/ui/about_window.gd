1322fe2e4c4407bb7ba245218b6107562f38836c
Krzyhau <krzyht@gmail.com>
2024-02-26 18:05:03 +0100

    add version to about window

--- scripts/ui/about_window.gd
+++ scripts/ui/about_window.gd
@@ -1,8 +1,13 @@
 extends Window
 
+@export var version_text : Label
+
 func _ready() -> void:
 	go_back_requested.connect(_close_window)
 	close_requested.connect(_close_window)
+	
+	version_text.text = "Version "
+	version_text.text += ProjectSettings.get_setting("application/config/version")
 
 func _close_window():
 	visible = false
